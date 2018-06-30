03da18c43897b62e5686d950c78973999e493d01
yousei <shamray.anna@gmail.com>
2018-06-30 14:23:30 +0300

    task 2.1 attemp 4

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-s
+
 sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
