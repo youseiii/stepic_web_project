c02165302ec3be9235aafff60f0767f49d0656db
yousei <shamray.anna@gmail.com>
2018-06-30 13:40:49 +0300

    task 2.1 attemp 3

--- init.sh
+++ init.sh
@@ -1,10 +1,6 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/gunicorn_hello.conf /etc/gunicorn.d/test_hello
-sudo ln -sf /home/box/web/etc/gunicorn_django.conf /etc/gunicorn.d/test_django
-
-#source /home/box/web/venv/bin/activate
-
-#gunicorn3 -c /home/box/web/etc/gunicorn_hello.conf  hello:app
-gunicorn3 -c /home/box/web/etc/gunicorn_django.conf  ask.wsgi:application
-
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+s
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo /etc/init.d/gunicorn restart
