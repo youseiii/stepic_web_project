e953e567b20c671932b6d5af241dc359ea01c70c
yousei <shamray.anna@gmail.com>
2018-08-12 21:29:58 +0300

    task 2.5 attempt 3

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ask',
-    'qa',
-    # 'django_faker'
+    'qa'
 ]
 
 MIDDLEWARE = [
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 
 # Create your models here.
@@ -16,7 +17,7 @@ class Question (models.Model):
 
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateField(null=True)
+    added_at = models.DateField(null=True, default=datetime.now)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name = 'q_to_likes')
@@ -30,7 +31,7 @@ class Question (models.Model):
 
 class Answer (models.Model):
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateField(null=True, default=datetime.now)
     question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
